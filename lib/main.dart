1be12c1e6af7c704e3134f1d414388373362f3f8
Luis Figueroa <figueroa.ides@gmail.com>
2019-07-14 16:53:47 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:pokedex/pokemon.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Pokedex",
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+// whenever there is data change or you are pulling data
+// you need to use a stateful widget
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+  PokeHub pokeHub;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // get the data
+    fetchData();
+  }
+
+  // use asynchronous
+  fetchData() async {
+    var res = await http.get(url);
+    var decodedJson = jsonDecode(res.body);
+
+    pokeHub = PokeHub.fromJson(decodedJson);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // add the app bar
+      appBar: AppBar(
+        title: Text("Pokedex"),
+        backgroundColor: Colors.red,
+      ),
+
+      // add the body
+      // because we're fetching data
+      // we need to include a loading button, if the ui gets loaded first
+      // and there's nothing to show, then we will get an error
+      // so if its null, then give a loading circle
+      body: pokeHub == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              children: pokeHub.pokemon
+                  .map((poke) => Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Card(
+                          elevation: 3.0,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Container(
+                                height: 100.0,
+                                width: 100.0,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: NetworkImage(poke.img))),
+                              ),
+                              Text(
+                                poke.name,
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
+      //drawer: Drawer(),
+    );
+  }
+}
